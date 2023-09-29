e2e452cc5ebe13d5d3ddb55ead958303855606e0
Victor Tran <victor.tran@tempestas.io>
2023-09-28 23:56:40 -0400

    remove boilerplate, add nav, link styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import Navbar from "~/components/layout/Navbar";
+import styles from '../styles/root.css'
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,17 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+  ];
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      {/* on top */}
+      <Navbar />
     </div>
   );
 }
