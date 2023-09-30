1bccc64f742f9d61f8316c2e77ae8b905d76dc3d
Victor Tran <victor.tran@tempestas.io>
2023-09-30 06:16:28 -0400

    create portfolio page skeleton

---
+++ app/routes/portfolio.tsx
@@ -0,0 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => {
+  return []
+}
+
+export default function Portfolio() {
+  return (
+    <section className="Portfolio"></section>
+  )
+}
