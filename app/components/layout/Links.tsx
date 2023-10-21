540461524543cd85e68eb7d51d27b2e2f48c2c83
Victor Tran <victor.tran@tempestas.io>
2023-10-21 05:47:41 -0400

    formatter

--- app/components/layout/Links.tsx
+++ app/components/layout/Links.tsx
@@ -1,19 +1,19 @@
-import Button from "../shared/button";
+import Button from '../shared/button';
 
 interface LinksProps {
   className?: string;
 }
 
-const Links = ({ className = "" }: LinksProps) => (
+const Links = ({ className = '' }: LinksProps) => (
   <div className={`Links ${className} flex flex-col gap-4`.trim()}>
     {[
       {
-        text: "Portfolio & Experience",
-        navigateTo: "/portfolio",
+        text: 'Portfolio & Experience',
+        navigateTo: '/portfolio',
       },
-      { text: "Contact & Socials", navigateTo: "/contact" },
-      { text: "Lore & About", navigateTo: "/about" },
-      { text: "Pet", navigateTo: "/pet" },
+      { text: 'Contact & Socials', navigateTo: '/contact' },
+      { text: 'Lore & About', navigateTo: '/about' },
+      { text: 'Pet', navigateTo: '/pet' },
     ].map(({ text, navigateTo }) => {
       return <Button key={text} text={text} navigateTo={navigateTo} />;
     })}
