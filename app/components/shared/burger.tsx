6e707fca970fb318fba87982fe75b0704bc48d8d
Victor Tran <victor.tran@tempestas.io>
2023-10-21 05:49:12 -0400

    update burger to be an icon from heroicon

--- app/components/shared/burger.tsx
+++ app/components/shared/burger.tsx
@@ -2,24 +2,19 @@ interface IBurgerProps {
   className?: string;
 }
 
-const Burger = ({ className = "", ...props }: IBurgerProps) => {
+const Burger = ({ className = '', ...props }: IBurgerProps) => {
   return (
-    <div className={(className + " Burger").trimStart()} {...props}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-12 h-16 text-[#fff]"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-      </svg>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6">
+      <path
+        fillRule="evenodd"
+        d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+        clipRule="evenodd"
+      />
+    </svg>
   );
 };
 
