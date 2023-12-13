0b7101c3d4a803bc3295f9d922aa25dd9ff7f6cd
Victor <victor.tran@tempestas.io>
2023-12-13 02:13:23 -0500

    update divide line

--- app/components/layout/Introduction.tsx
+++ app/components/layout/Introduction.tsx
@@ -13,7 +13,7 @@ const Introduction = ({
     <section
       className={(
         className +
-        ' divide-y divide-blue-500 my-2 text-center md:w-1/2 md:mx-auto'
+        'Introduction divide-y divide-blue-500 my-2 text-center md:w-3/4 md:mx-auto'
       ).trimStart()}>
       {children}
       {introText && <div className="font-bold pt-2">{introText}</div>}
