8641619892218ba4ff38a53da2cff7be30884b93
Victor <victor.tran@tempestas.io>
2023-12-13 04:55:51 -0500

    remove unused style

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,17 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
-
-import Main from "~/components/layout/Main";
+import Main from '~/components/layout/Main';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Victor Tran | Portfolio" },
-    { name: "description", content: "Victor's personal project portfolio, inspired by JJK" },
+    { title: 'Victor Tran | Portfolio' },
+    {
+      name: 'description',
+      content: "Victor's personal project portfolio, inspired by JJK",
+    },
   ];
 };
 
-
 export default function Index() {
-  return (
-      <Main className="h-auto"/>
-  );
+  return <Main />;
 }
