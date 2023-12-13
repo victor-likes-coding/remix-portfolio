c843cc0b8ee0a22c120f50b5a3278ba5f505dfbd
Victor <victor.tran@tempestas.io>
2023-12-13 04:02:29 -0500

    remove w-1/2 for w-full

--- app/components/shared/button.tsx
+++ app/components/shared/button.tsx
@@ -11,7 +11,7 @@ const Button = ({ className = '', text, navigateTo }: ButtonProps) => {
     <Link
       className={(
         className +
-        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:w-1/2 md:border-x-0 md:border-l-2 last-of-type:md:border-r-2'
+        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:border-x-0 md:border-l-2 last-of-type:md:border-r-2 md:w-full'
       ).trim()}
       to={navigateTo}>
       {text
