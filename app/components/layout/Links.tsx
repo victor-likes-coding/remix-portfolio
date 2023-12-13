efc6cdef3d47ba13c12f1022537ae6f8722e91ff
Victor <victor.tran@tempestas.io>
2023-12-13 01:56:44 -0500

    style links

--- app/components/layout/Links.tsx
+++ app/components/layout/Links.tsx
@@ -6,7 +6,7 @@ interface LinksProps {
 
 const Links = ({ className = '' }: LinksProps) => (
   <div
-    className={`Links ${className} flex flex-col md:flex-row gap-4 md:gap-0 md:w-1/2 md:flex-wrap md:mx-auto`.trim()}>
+    className={`Links ${className} flex flex-col md:flex-row gap-4 md:gap-0 md:mx-auto md:w-3/4 md:text-sm`.trim()}>
     {[
       {
         text: 'Portfolio & Experience',
--- app/components/shared/button.tsx
+++ app/components/shared/button.tsx
@@ -11,10 +11,16 @@ const Button = ({ className = '', text, navigateTo }: ButtonProps) => {
     <Link
       className={(
         className +
-        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:border-l-0 md:w-1/2 md:odd:border-l-2'
+        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:w-1/2 md:border-x-0 md:border-l-2 last-of-type:md:border-r-2'
       ).trim()}
       to={navigateTo}>
-      {text ? text : 'Button'}
+      {text
+        ? text.split(' ').map((value, index) => (
+            <span key={index} className={index === 0 ? '' : 'md:hidden'}>
+              {value}{' '}
+            </span>
+          ))
+        : 'Button'}
     </Link>
   );
 };
