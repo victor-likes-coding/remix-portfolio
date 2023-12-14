08dfa9257cb4558e4b112fec94a6b6cb41422052
Victor <victor.tran@tempestas.io>
2023-12-13 21:46:17 -0500

    put className after string of classNames

--- app/components/shared/button.tsx
+++ app/components/shared/button.tsx
@@ -10,8 +10,8 @@ const Button = ({ className = '', text, navigateTo }: ButtonProps) => {
   return (
     <Link
       className={(
-        className +
-        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:border-x-0 md:border-l-2 last-of-type:md:border-r-2 md:w-full'
+        ' font-bold block rounded-none p-3 border-x-2 border-y-1 border-x-blue-500 border-y-black text-center bg-slate-900 hover:bg-blue-700 focus:bg-blue-700 active:bg-white active:text-black hover:text-black focus:text-black Button md:border-x-0 md:border-l-2 last-of-type:md:border-r-2 md:w-full' +
+        className
       ).trim()}
       to={navigateTo}>
       {text
